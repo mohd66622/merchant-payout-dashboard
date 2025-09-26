58ffaf2f85fcaa1315de3ce356abec5e56960dae
Mohd Touheed <mohdtouheed@gmail.com>
2025-09-26 17:39:40 +0530

    renamed index.js to server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
-
+ 
 const connectDB = require('./config');
 connectDB(); // Connect to MongoDB
 
